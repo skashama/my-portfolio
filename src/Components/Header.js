5e9e5ba19a1fed263bb2b3dc9495be0d87e1872a
skashama <shaloombowa@gmail.com>
2020-07-21 22:53:36 -0700

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <div class="intro">
+            <div class="container">
+            <div class="row">
+                <div class="intro-text">
+                <h1>Hello, I'm <span class="name">Shaloom Kashama</span></h1>
+                <p>Full Stack Web Developer</p>
+                <a href="#about" class="btn btn-default btn-lg page-scroll">More about me</a> </div>
+            </div>
+            </div>
+            </div>
+        </div>
+    )
+}
